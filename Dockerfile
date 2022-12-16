23e8041b9f3f57cb1a798138e6f149d38a643a4e
technillogue <technillogue@gmail.com>
2022-12-16 18:29:16 -0500

    add ws only

--- Dockerfile
+++ Dockerfile
@@ -22,7 +22,7 @@ COPY --from=ait /workdir/tmp /app/tmp
 COPY --from=model /root/.cache /root/.cache
 COPY --from=libbuilder /app/venv/lib/python3.10/site-packages /app/
 COPY ./modeling/ /app/modeling
-COPY ./pipeline_stable_diffusion_ait.py ./client.js ./index.html ./server.py /app/
+COPY ./pipeline_stable_diffusion_ait.py ./client.js ./index.html ./ws-only.html ./server.py /app/
 ENV DISABLE_TELEMETRY=YES
 EXPOSE 8080
 ENTRYPOINT ["/usr/local/bin/python3.10", "/app/server.py"]
--- server.py
+++ server.py
@@ -170,6 +170,9 @@ class Live:
         # resp.enable_compression(force=True)
         return resp
 
+    async def ws_only(self, req: web.Request) -> web.Response:
+        return web.FileResponse("./ws-only.html")
+
 
 app = web.Application()
 live = Live()
@@ -180,6 +183,7 @@ app.add_routes(
         web.route("*", "/client.js", live.js),
         web.post("/offer", live.offer),
         web.get("/ws", live.handle_ws),
+        web.get("ws-only.html", live.ws_only),
         web.post(
             "/v1alpha/generation/stable-diffusion-512-v2-0/text-to-image",
             live.handle_endpoint,
